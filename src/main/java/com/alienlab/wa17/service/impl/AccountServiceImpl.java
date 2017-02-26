27affe8f027079013acf142d957a3dde722751ef
juhuiguang <juhuiguang@foxmail.com>
2017-02-26 15:32:36 +0800

    集成swagger2

---
+++ src/main/java/com/alienlab/wa17/service/impl/AccountServiceImpl.java
@@ -0,0 +1,14 @@
+package com.alienlab.wa17.service.impl;
+
+import com.alienlab.wa17.entity.main.MainTbAccount;
+import com.alienlab.wa17.service.AccountService;
+
+/**
+ * Created by juhuiguang on 2017/2/25.
+ */
+public class AccountServiceImpl implements AccountService {
+    @Override
+    public MainTbAccount getAccount(String account_loginname) throws Exception {
+        return null;
+    }
+}
