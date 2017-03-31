3cf639b0fe251aa35e35f14340b75944aee02f22
juhuiguang <juhuiguang@foxmail.com>
2017-04-01 01:03:27 +0800

    新增下单数据接口

--- src/main/java/com/alienlab/wa17/service/impl/OrderServiceImpl.java
+++ src/main/java/com/alienlab/wa17/service/impl/OrderServiceImpl.java
@@ -2,11 +2,16 @@ package com.alienlab.wa17.service.impl;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.util.TypeUtils;
 import com.alienlab.wa17.dao.DaoTool;
 import com.alienlab.wa17.entity.client.ClientTbCustom;
 import com.alienlab.wa17.entity.client.dto.OrderDto;
 import com.alienlab.wa17.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
 
 /**
  * Created by 橘 on 2017/3/31.
@@ -36,11 +41,27 @@ public class OrderServiceImpl implements OrderService {
                     odd=detail.getFloat("total_price");
                 }else if(productName.equals("核销")&&detail.getFloat("total_price")>0){//处理核销
                     recharge=detail.getFloat("total_price");
-                }else{//一般商品的请求
+                }else{//一般商品的请求,验证库存是否可以满足下单
 
                 }
             }
         }
         return null;
+    }
+
+    private String getOrderNo(int account,long shopId) throws Exception {
+        SimpleDateFormat format=new SimpleDateFormat("yyyyMMdd");
+        String ordernopre=account+"-"+format.format(new Date())+"-"+shopId;
+        String sql="select count(1) num from tb_order where shop_id="+shopId+" and order_code like '"+ordernopre+"%'";
+        Map<String,Object> map=daoTool.getMap(sql,account);
+        int no=TypeUtils.castToInt(map.get("num"));
+        String result=ordernopre+"-"+(no+1);
+        return result;
+    }
+
+    private boolean validateInventory(int account,long shopId,long skuid,Float saleAmount) throws Exception {
+        String sql="select * from tb_inventory where sku_id="+skuid+" and shop_id="+shopId;
+        Map<String,Object> map=daoTool.getMap(sql,account);
+        return false;
     }
 }
