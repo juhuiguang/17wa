52ed1e608e475b2ff6ae27914dea7a39f5810532
juhuiguang <juhuiguang@foxmail.com>
2017-07-04 11:58:59 +0800

    增加盘点数字临时保存

---
+++ src/main/java/com/alienlab/wa17/entity/client/ClientTbInventoryTemp.java
@@ -0,0 +1,68 @@
+package com.alienlab.wa17.entity.client;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Id;
+import java.sql.Timestamp;
+
+/**
+ * Created by 橘 on 2017/7/4.
+ */
+public class ClientTbInventoryTemp {
+    private long id;
+    private Long skuId;
+    private Long shopId;
+    private Integer inventoryAmount;
+    private Timestamp inventoryCountTime;
+
+    @Id
+    @Column(name = "id")
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "sku_id")
+    public Long getSkuId() {
+        return skuId;
+    }
+
+    public void setSkuId(Long skuId) {
+        this.skuId = skuId;
+    }
+
+    @Basic
+    @Column(name = "shop_id")
+    public Long getShopId() {
+        return shopId;
+    }
+
+    public void setShopId(Long shopId) {
+        this.shopId = shopId;
+    }
+
+    @Basic
+    @Column(name = "amount")
+    public Integer getInventoryAmount() {
+        return inventoryAmount;
+    }
+
+    public void setInventoryAmount(Integer inventoryAmount) {
+        this.inventoryAmount = inventoryAmount;
+    }
+
+    @Basic
+    @Column(name = "temp_time")
+    public Timestamp getInventoryCountTime() {
+        return inventoryCountTime;
+    }
+
+    public void setInventoryCountTime(Timestamp inventoryCountTime) {
+        this.inventoryCountTime = inventoryCountTime;
+    }
+
+}
